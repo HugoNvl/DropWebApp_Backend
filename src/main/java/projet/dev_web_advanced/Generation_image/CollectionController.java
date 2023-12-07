7ed4575ee9c54c87b62a7ce41aa0274c49c76863
HugoNvl <hugo.nouvellon@orange.fr>
2023-12-07 14:13:01 +0100

    connection with front

--- src/main/java/projet/dev_web_advanced/Generation_image/CollectionController.java
+++ src/main/java/projet/dev_web_advanced/Generation_image/CollectionController.java
@@ -5,7 +5,11 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class CollectionController {
     
     private CollectionDAO dao;
